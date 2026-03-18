6c8f13299ab1f74b28919b0f3f4df2046102ef89
batistalando7 <batistalando7@gmail.com>
2026-03-18 18:56:18 +0100

    controllers configurados para o user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,40 +37,42 @@ class UserController extends Controller
 
     public function show($id)
     {
-        //
+        $response['user'] = User::findOrFail($id);
+        return view('admin.user.details.index', $response);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        $response = User::findOrFail($id);
+        return view('admin.user.edit', $response);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+         $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8'],
+            'role' => ['required', 'string']
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->role = $request->role;
+
+        $user->update();
+
+        return redirect()->back()->with('success', 'Atualizado com Sucesso!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+        
+        return redirect()->back()->with('success', 'Deletado com Sucesso!');
     }
 }
